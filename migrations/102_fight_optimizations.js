563629dc065b2320c652eb58791cb30903044b31
Diego Vidaurre <d.vidaurres@gmail.com>
2021-11-15 18:26:14 -0300

    add mirations and newtwork files

--- migrations/102_fight_optimizations.js
+++
@@ -1,5 +0,0 @@
-// yo busco duelo, pillo (sin poner perform duel)
-// hago perform duel
-// quedo esperando el bot, el isAttackable puede acabarse justo cuando estoy esperando el duelo
-// alguin buscando el duelo me puede agarrar a mi justo ahi
-// puede quedar un duelo podrido y se crea otro duelo
