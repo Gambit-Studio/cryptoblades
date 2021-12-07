3ed0d9921d26a45ba86396e41c053e6715879ba4
Cristóbal Rementería <cristobal.rementeria@gmail.com>
2021-12-07 13:06:44 -0300

    improve: pvp main view paddings

--- frontend/src/components/smart/pvp-overhaul/PvP.vue
+++ frontend/src/components/smart/pvp-overhaul/PvP.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="pvpWrapper">
     <pvp-nav-bar @changeTab="onChangeTab" :isMatchmakingActive="false" />
-    <div v-if="tab === 1">
-      <!-- <pvp-arena-preparation :character="this.character" :opponent="this.opponent"/> -->
-      <!-- <pvp-arena-summary :character="this.character" :opponent="this.opponent"/> -->
+    <div v-if="tab === 0">
+      <pvp-arena-preparation :character="this.character" :opponent="this.opponent"/>
+      <pvp-arena-summary :character="this.character" :opponent="this.opponent"/>
       <pvp-arena-matchmaking :character="this.character" :opponent="this.opponent"/>
     </div>
-    <div v-if="tab === 0">
+    <div v-if="tab === 1">
       <pvp-leaderboards />
     </div>
   </div>
 </template>
 
 <script>
-// import PvPArenaPreparation from './arena/PvPArenaPreparation.vue';
-// import PvPArenaSummary from './arena/PvPArenaSummary.vue';
+import PvPArenaPreparation from './arena/PvPArenaPreparation.vue';
+import PvPArenaSummary from './arena/PvPArenaSummary.vue';
 import PvPArenaMatchMaking from './arena/PvPArenaMatchMaking.vue';
 import PvPNavBar from './components/PvPNavBar.vue';
 import PvPLeaderboards from './leaderboards/PvPLeaderboards.vue';
@@ -23,11 +23,11 @@ import PvPLeaderboards from './leaderboards/PvPLeaderboards.vue';
 export default {
   components: {
     'pvp-nav-bar': PvPNavBar,
-    // 'pvp-modal': PvPModal
-    // 'pvp-arena-preparation': PvPArenaPreparation,
-    // 'pvp-arena-summary': PvPArenaSummary,
+    'pvp-arena-preparation': PvPArenaPreparation,
+    'pvp-arena-summary': PvPArenaSummary,
     'pvp-arena-matchmaking': PvPArenaMatchMaking,
     'pvp-leaderboards': PvPLeaderboards
+    // 'pvp-modal': PvPModal
   },
   data() {
     return {
@@ -56,6 +56,22 @@ export default {
 
 <style scoped lang="scss">
 .pvpWrapper {
-  padding: 0 4rem;
+  padding: 0 2rem;
+
+  @media screen and (min-width: 1240px) {
+    padding: 0 4rem;
+  }
+
+  @media screen and (min-width: 1440px) {
+    padding: 0 6rem;
+  }
+
+  @media screen and (min-width: 1680px) {
+    padding: 0 8rem;
+  }
+
+  @media screen and (min-width: 2560px) {
+    padding: 0 10rem;
+  }
 }
 </style>
