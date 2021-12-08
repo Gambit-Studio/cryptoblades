9aa52ca0e16a3c73f979298e8ebc1b3f41742dd7
Cristóbal Rementería <cristobal.rementeria@gmail.com>
2021-12-08 15:31:37 -0300

    wip: add arenamatchmaking props

--- frontend/src/components/smart/pvp-overhaul/pvp-arena/PvPArena.vue
+++ frontend/src/components/smart/pvp-overhaul/pvp-arena/PvPArena.vue
@@ -4,53 +4,37 @@
       LOADING!
     </div>
     <div v-else>
-      <div>{{ currentCharacterId }}</div>
-      <pvp-arena-preparation v-if="!isCharacterInArena" />
-      <!-- <pvp-arena-summary v-if="isCharacterInArena" :characterName="asd"/> -->
-      <pvp-arena-matchmaking v-if="false" />
+      <div>{{ currentCharacter }}</div>
+      <!-- <pvp-arena-preparation v-if="!isCharacterInArena" />
+      <pvp-arena-summary v-if="isCharacterInArena" :characterName="asd"/> -->
+      <pvp-arena-matchmaking v-if="!isCharacterInArena" :characterName="nombredelweon" />
     </div>
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex';
-import PvPArenaPreparation from './sub-components/PvPArenaPreparation.vue';
+import { getCharacterNameFromSeed } from '@/character-name';
+import { mapState } from 'vuex';
+// import PvPArenaPreparation from './sub-components/PvPArenaPreparation.vue';
 // import PvPArenaSummary from './sub-components/PvPArenaSummary.vue';
 import PvPArenaMatchMaking from './sub-components/PvPArenaMatchMaking.vue';
 
 export default {
   components: {
-    'pvp-arena-preparation': PvPArenaPreparation,
+    // 'pvp-arena-preparation': PvPArenaPreparation,
     // 'pvp-arena-summary': PvPArenaSummary,
     'pvp-arena-matchmaking': PvPArenaMatchMaking
   },
-
   data() {
     return {
       loading: true,
       isCharacterInArena: false,
       isMatchMaking: false,
+      nombredelweon: '',
     };
   },
-
   computed: {
     ...mapState(['currentCharacterId', 'contracts', 'defaultAccount']),
-    ...mapGetters([
-      'currentCharacter',
-      'currentCharacterStamina',
-      'getCharacterName',
-      'getCharacterStamina',
-      'charactersWithIds',
-      'ownCharacters',
-      'timeUntilCharacterHasMaxStamina',
-      'getIsInCombat',
-      'getIsCharacterViewExpanded',
-      'fightGasOffset',
-      'fightBaseline'
-    ]),
-    asd() {
-      return this.getCharacterName(this.currentCharacterId);
-    }
   },
 
   methods: {
@@ -62,6 +46,7 @@ export default {
       if (await this.contracts().PvpArena.methods.isCharacterInArena(this.currentCharacterId).call({ from: this.defaultAccount })) {
         this.isCharacterInArena = true;
       }
+      this.nombredelweon = getCharacterNameFromSeed(this.currentCharacterId);
     }
     this.$emit('isCharacterInArena', this.isCharacterInArena);
     this.loading = false;
@@ -77,6 +62,7 @@ export default {
         } else {
           this.isCharacterInArena = false;
         }
+        this.nombredelweon = getCharacterNameFromSeed(this.currentCharacterId);
       }
 
       this.loading = false;
